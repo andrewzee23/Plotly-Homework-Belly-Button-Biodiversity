3cc7ef1ddf85819da9a4c2ae4521a3dab5a4deaa
andrewzee23 <andrewazamora@gmail.com>
2020-10-20 21:29:45 -0500

    created dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,4 +3,14 @@ d3.json("samples.json").then(function createPlotly(data) {
     console.log(data);
     var testid = data.names;
     console.log(testid);
+
+// Create a dynamic dropdown menu
+  d3.select("#selDataset")
+  .selectAll("option")
+  .data(testid)
+  .enter()
+  .append("option")
+  .html(function(d) {
+    return `<option>${d}</option`;
+  });
 });
